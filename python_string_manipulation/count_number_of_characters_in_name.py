809e6f97c8366c1cb581ceb132a5c0004226893e
Mark Comia <marklestercomia22@gmail.com>
2025-03-21 12:52:04 +0800

    counted the number of char and then print it afterwards, added another line of pseudocode

---
+++ python_string_manipulation/count_number_of_characters_in_name.py
@@ -0,0 +1,6 @@
+#ask user to input fullname
+fullname = input('Enter Your Fullname: ')
+#count the number of characters
+character_count = len(fullname)
+#print number of characters
+print(f'The number of characters is: {character_count} characters')
