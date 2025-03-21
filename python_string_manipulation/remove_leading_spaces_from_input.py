c14ea710497cdd2cfcfa9f3392744f4037b73611
Mark Comia <marklestercomia22@gmail.com>
2025-03-21 11:22:47 +0800

    print the user full name without lead spaces with lstrip

--- python_string_manipulation/remove_leading_spaces_from_input.py
+++ python_string_manipulation/remove_leading_spaces_from_input.py
@@ -1,4 +1,5 @@
 #ask for the user name with spaces at beginning
-fullname = input('Enter your full name:')
+fullname = input('Enter your full name:            ')
 
-#remove the leading spaces
+#print fullname and remove the leading spaces
+print('User fullname:' , fullname.lstrip())
