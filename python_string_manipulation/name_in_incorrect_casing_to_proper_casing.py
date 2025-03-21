266db47bcaf5acab096da5a226f743cc8e5ac1a9
Mark Comia <marklestercomia22@gmail.com>
2025-03-21 12:12:35 +0800

    ask for incorrect casing then fix it using title

---
+++ python_string_manipulation/name_in_incorrect_casing_to_proper_casing.py
@@ -0,0 +1,4 @@
+#ask for user full name in incorrect casing
+fullname = input('Enter your Fullname in incorrect casing: ')
+#print name in proper casing
+print('Name in correct casing: ' , fullname.title())
